4149ce62270ab306304a4af08fe6b67a384b3acb
caguilarhernandez <144890718+caguilarhernandez@users.noreply.github.com>
2024-02-18 21:53:56 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,1 +1,15 @@
+<html>
 
+<head>
+
+    <title>Actividad 1.3.1</title>
+
+</head>
+
+<body>
+
+    <?php echo "<h1>Actividad 1.3.1: LAMP Sever. Autor: Carlos Aguilar Hernández</h1>"; ?>   
+
+</body>
+
+</html>
